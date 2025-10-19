9baaa8370acb719e1dc1370cd95cf5a8dc1f33d0
tomyyyD <thomas5d@comcast.net>
2025-10-19 19:32:33 -0400

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.API_HOST || 'http://localhost:3000',
+          target: env.API_HOST,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, '')
         }
